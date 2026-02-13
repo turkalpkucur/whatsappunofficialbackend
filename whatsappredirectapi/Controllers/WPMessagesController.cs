5bae08ae8030a500e09938ebb118848f6baad040
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2026-02-13 16:09:00 +0300

    güncelleme
    güncelleme

--- whatsappredirectapi/Controllers/WPMessagesController.cs
+++ whatsappredirectapi/Controllers/WPMessagesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Npgsql;
 using System.Net.Http.Headers;
 using System.Text;
 using static whatsappredirectapi.Models.ConnexeaseSendMessageModel;
@@ -11,7 +12,7 @@ namespace whatsappredirectapi.Controllers
     public class WPMessagesController : ControllerBase
     {
 
-        [HttpPost("sendwhatsappmessage")]
+        [HttpPost("sendwhatsappmessageviaconnexease")]
         public async Task<IActionResult> SendWhatsappMessage([FromQuery] string message)
         {
             string BASE_API_URL = "https://api.connexease.com";
@@ -48,5 +49,47 @@ type="text"
             //CxResponse resp = JsonConvert.DeserializeObject<CxResponse>(responseContent);
             return Ok();
         }
+
+
+
+        [HttpPost("webhooksendmessageunofficial")]
+        public async Task<IActionResult> WebhookSendMessageUnofficial([FromQuery] string message)
+        {
+
+
+
+            //using (NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;Database=wease_dev_localdb;User Id=postgres;Password=Wease@47"))
+            //{
+            //    StringBuilder query = new StringBuilder(" INSERT INTO wease.leads(\r\n\t  company_id, user_id, lead_no, lead_status_id, lead_sub_status_id, lead_reference_id, first_name, last_name, gender, dob, country_phone_code, phone_number, email, profile_picture, assigned_at, lost_status_break_down_id, lost_status_break_down_description, language_codes_lead_knows, is_completed, is_active, created_at, deleted_at, starred, lead_type, time_zone_id, meta_ad_id, is_logistic_detail_required)\r\n\tVALUES ( 1, null, 9999, 1, null, 13, 'HAYDAR', 'NAMLI', 1, '1980-01-05', 218, '905054270123', 'turkalp.kucur@gmail.com', null, null,null,null, null, false,true,  NOW(), null,false, 1,null,null,false);  ");
+            //    con.Open();
+            //    NpgsqlCommand cmd = (NpgsqlCommand)con.CreateCommand();
+            //    NpgsqlParameter param = new NpgsqlParameter("@companyId", -1);
+
+            //    cmd.Parameters.Add(param);
+            //    cmd.CommandText = query.ToString();
+
+            //    NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            //    while (reader.Read())
+            //    {
+            //        InsurancePlanPrice item = new InsurancePlanPrice();
+            //        if (reader["insurance_plan_price_id"] != DBNull.Value)
+            //            item.Id = Convert.ToInt32(reader["insurance_plan_price_id"]);
+
+            //        result.Add(item);
+            //    }
+            //}
+
+
+
+
+
+
+
+
+            return Ok();
+        }
     }
 }
+
+//
